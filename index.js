4042ed4188396f24bcf1f0432c9d71dbc16ef251
jjuan0848 <2314894808@qq.com>
2022-09-17 22:58:30 +0800

    text

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const morgan = require("morgan");
 const { init: initDB, Counter } = require("./db");
 
+const Tesseract = require('tesseract.js');
+
 const logger = morgan("tiny");
 
 const app = express();
@@ -12,10 +14,31 @@ app.use(express.json());
 app.use(cors());
 app.use(logger);
 
+const worker = Tesseract.createWorker({
+  logger: m => console.log(m),
+  errorHandler: err => console.log('[error:]', err),
+});
+
 // 首页
 app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
+
+const workerFunc = async () => {
+  await worker.load();
+  await worker.loadLanguage('eng');
+  await worker.initialize('eng', OEM.LSTM_ONLY);
+  await worker.setParameters({
+    tessedit_pageseg_mode: PSM.SINGLE_BLOCK,
+  });
+
+  // const image =  require('fs').readFileSync('./donate_wx.png');
+  const image = 'https://lzw.me/wp-content/uploads/2017/02/donate_wx.png';
+  const { data: { text } } = await worker.recognize(image);
+  console.log(text);
+  await worker.terminate();
+  return text;
+}
 
 // 更新计数
 app.post("/api/count", async (req, res) => {
@@ -36,6 +59,15 @@ app.post("/api/count", async (req, res) => {
 // 获取计数
 app.get("/api/count", async (req, res) => {
   const result = await Counter.count();
+  res.send({
+    code: 0,
+    data: result,
+  });
+});
+
+// 获取图片中的文字
+app.get("/api/getText", async (req, res) => {
+  const result = await workerFunc();
   res.send({
     code: 0,
     data: result,
